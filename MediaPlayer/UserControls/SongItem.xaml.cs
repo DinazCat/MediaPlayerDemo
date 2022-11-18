1f5c67df1d6965e633761fe0ca24cb261e63c3e4
Cat Tuong <you@example.com>
2022-11-18 23:11:45 +0700

    rename project

---
+++ MediaPlayer/UserControls/SongItem.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SongItem.xaml
+    /// </summary>
+    public partial class SongItem : UserControl
+    {
+        public SongItem()
+        {
+            InitializeComponent();
+        }
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(SongItem));
+
+        public string Artist
+        {
+            get { return (string)GetValue(ArtistProperty); }
+            set { SetValue(ArtistProperty, value); }
+        }
+        public static readonly DependencyProperty ArtistProperty = DependencyProperty.Register("Artist", typeof(string), typeof(SongItem));
+        public string Album
+        {
+            get { return (string)GetValue(AlbumProperty); }
+            set { SetValue(AlbumProperty, value); }
+        }
+        public static readonly DependencyProperty AlbumProperty = DependencyProperty.Register("Album", typeof(string), typeof(SongItem));
+
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register("Time", typeof(string), typeof(SongItem));
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(SongItem));
+
+        public bool IsLiked
+        {
+            get { return (bool)GetValue(IsLikedProperty); }
+            set { SetValue(IsLikedProperty, value); }
+        }
+        public static readonly DependencyProperty IsLikedProperty = DependencyProperty.Register("IsLiked", typeof(bool), typeof(SongItem));
+    }
+}
---
+++ MediaPlayer/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using MediaPlayer.Command;
+using MediaPlayer.View;
+
+namespace MediaPlayer.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {       
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UpdateViewCommand { get; set; }
+        public ICommand AddPlaylistWindowCommand { get; set; }
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                Isloaded = true;
+                //LoginWindow loginWindow = new LoginWindow();
+                //loginWindow.ShowDialog();
+            }
+              );
+            UpdateViewCommand = new UpdateViewCommand(this);
+            AddPlaylistWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                AddPlaylistWindow wd = new AddPlaylistWindow();
+                wd.ShowDialog();
+            });
+        }
+        private BaseViewModel _selectedViewModel = new HomeViewModel();
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set { 
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));            
+            }
+        }
+    }
+}
